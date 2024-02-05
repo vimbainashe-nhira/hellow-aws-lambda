a64639709c4d90b95e3a3204e5987b157a98da22
Vimbainashe Nhira <nhiralee@gmail.com>
2024-02-05 15:36:38 +0100

    initial commit

---
+++ src/main/java/com/nashe/hellowawslambda/HellowAwsLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.nashe.hellowawslambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowAwsLambdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowAwsLambdaApplication.class, args);
+    }
+
+}
